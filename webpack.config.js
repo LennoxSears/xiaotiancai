c5e5009d18f4b7fa4c37f29dcc751fe561964f60
lennoxge <zhendong.ge@yahoo.com>
2021-01-22 16:44:14 +0800

    title adjust

--- webpack.config.js
+++ webpack.config.js
@@ -146,7 +146,7 @@ module.exports = [
             new HtmlWebpackPlugin({
                 chunks: ['lib.min', 'gui'],
                 template: 'src/playground/index.ejs',
-                title: 'Scratch 3.0 GUI',
+                title: '小天才积木编程DEMO',
                 sentryConfig: process.env.SENTRY_CONFIG ? '"' + process.env.SENTRY_CONFIG + '"' : null
             }),
             new HtmlWebpackPlugin({
